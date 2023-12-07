d6e25e5a711d29e4aca26adaeb513b46c0c00e56
Daisy <345@gmail.com>
2023-12-07 12:05:02 +0800

    the first section finished

---
+++ src/components/About.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants/constants.js";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}></motion.div>
+    </>
+  );
+};
+
+export default About;
